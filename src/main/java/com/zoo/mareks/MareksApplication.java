7127ca2460396ebb0764f7bbcdca2a58abd95b14
MareksKing <marcins311@inbox.lv>
2022-06-10 15:11:19 +0300

    initial commit

---
+++ src/main/java/com/zoo/mareks/MareksApplication.java
@@ -0,0 +1,13 @@
+package com.zoo.mareks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MareksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MareksApplication.class, args);
+	}
+
+}
